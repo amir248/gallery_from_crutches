717cd3a1df1d7f25dc32d3064aacbf39af6fa250
Amir <chikchicly@gmail.com>
2021-12-07 10:43:05 +0700

    ButtonClick

---
+++ gallery-js-css/buttonClick.js
@@ -0,0 +1,33 @@
+document.querySelector('#buttonLeft').addEventListener('click',()=>{
+  console.log("buttonLeftClick");
+  CountClick--;
+  if(CountClick<=0){
+    CountClick=IMG.length;
+    console.log(CountClick<IMG.length);
+  }
+    console.log("coutnClick"+'__'+CountClick);
+    for(let newi=0;newi<IMG.length;newi++){
+      if(IMG[newi].count==CountClick){
+        console.log('nu vot'+IMG[newi].count+'__'+CountClick);
+        document.querySelector('img').src=IMG[newi].src;
+        document.querySelector('img').setAttribute('alt',IMG[newi].alt);
+          window.location.hash='#'+IMG[newi].hash;
+      }
+    }
+});
+document.querySelector('#buttonRight').addEventListener('click',()=>{
+  console.log("buttonRightClick");
+  CountClick++;
+  if(CountClick>=IMG.length){
+    CountClick=0;
+  }
+    console.log("coutnClick"+'__'+CountClick);
+    for(let newi=0;newi<IMG.length;newi++){
+      if(IMG[newi].count==CountClick){
+        console.log('nu vot'+IMG[newi].count+'__'+CountClick);
+        document.querySelector('img').src=IMG[newi].src;
+        document.querySelector('img').setAttribute('alt',IMG[newi].alt);
+          window.location.hash='#'+IMG[newi].hash;
+      }
+    }
+});
